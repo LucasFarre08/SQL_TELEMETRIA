37cecd6350d5c6871aa339c01dee4a5e1e7fb7ac
LucasFarre08 <lucas.farre@buonnytech.com.br>
2026-07-13 15:17:37 -0300

    Update limpeza.sql

--- sql/limpeza.sql
+++ sql/limpeza.sql
@@ -26,56 +26,34 @@ SET
     ano_mes = DATE_FORMAT(ativado, '%Y%m'),
     grouping_ano_mes = CONCAT(`grouping`, DATE_FORMAT(ativado, '%Y%m'));
 
+UPDATE freio
+SET
+    ano_mes = DATE_FORMAT(ativado, '%Y%m'),
+    grouping_ano_mes = CONCAT(`grouping`, DATE_FORMAT(ativado, '%Y%m'));
+UPDATE ociosidade
+SET
+    ano_mes = DATE_FORMAT(ativado, '%Y%m'),
+    grouping_ano_mes = CONCAT(`grouping`, DATE_FORMAT(ativado, '%Y%m'));
+UPDATE seguranca
+SET
+    ano_mes = DATE_FORMAT(ativado, '%Y%m'),
+    grouping_ano_mes = CONCAT(`grouping`, DATE_FORMAT(data, '%Y%m'));
+UPDATE rpm_amerelo
+SET
+    ano_mes = DATE_FORMAT(ativado, '%Y%m'),
+    grouping_ano_mes = CONCAT(`grouping`, DATE_FORMAT(ativado, '%Y%m'));
+UPDATE rpm_vermelho
+SET
+    ano_mes = DATE_FORMAT(ativado, '%Y%m'),
+    grouping_ano_mes = CONCAT(`grouping`, DATE_FORMAT(ativado, '%Y%m'));
+UPDATE velocidade_80km
+SET
+    ano_mes = DATE_FORMAT(ativado, '%Y%m'),
+    grouping_ano_mes = CONCAT(`grouping`, DATE_FORMAT(ativado, '%Y%m'));
+UPDATE velocidade_chuva_60km
+SET
+    ano_mes = DATE_FORMAT(ativado, '%Y%m'),
+    grouping_ano_mes = CONCAT(`grouping`, DATE_FORMAT(ativado, '%Y%m'));
 
-ALTER TABLE freio
-ADD COLUMN ano_mes VARCHAR(6);
-
-ALTER TABLE freio
-ADD COLUMN grouping_ano_mes VARCHAR(255);
-
-ALTER TABLE ociosidade
-ADD COLUMN ano_mes VARCHAR(6);
-
-ALTER TABLE ociosidade
-ADD COLUMN grouping_ano_mes VARCHAR(255);
-
-ALTER TABLE embreagem
-ADD COLUMN ano_mes VARCHAR(6);
-
-ALTER TABLE embreagem
-ADD COLUMN grouping_ano_mes VARCHAR(255);
-
-ALTER TABLE rpm_amarelo
-ADD COLUMN ano_mes VARCHAR(6);
-
-ALTER TABLE rpm_amarelo
-ADD COLUMN grouping_ano_mes VARCHAR(255);
-
-ALTER TABLE rpm_vermelho
-ADD COLUMN ano_mes VARCHAR(6);
-
-ALTER TABLE rpm_vermelho
-ADD COLUMN grouping_ano_mes VARCHAR(255);
-
-ALTER TABLE seguranca
-ADD COLUMN ano_mes VARCHAR(6);
-
-ALTER TABLE seguranca
-ADD COLUMN grouping_ano_mes VARCHAR(255);
-
-ALTER TABLE velocidade_80km
-ADD COLUMN ano_mes VARCHAR(6);
-
-ALTER TABLE velocidade_80km
-ADD COLUMN grouping_ano_mes VARCHAR(255);
-
-ALTER TABLE velocidade_chuva_60km
-ADD COLUMN ano_mes VARCHAR(6);
-
-ALTER TABLE velocidade_chuva_60km
-ADD COLUMN grouping_ano_mes VARCHAR(255);
-
-ALTER TABLE agregado_mensal
-ADD COLUMN grouping_ano_mes VARCHAR(255);
 
 
