86f975c2b8053be4fa287e045eeb0e5b2867039c
LucasFarre08 <lucas.farre@buonnytech.com.br>
2026-07-13 14:00:58 -0300

    Update limpeza.sql

--- sql/limpeza.sql
+++ sql/limpeza.sql
@@ -16,5 +16,15 @@ UPDATE viagens
 SET litros_consumidos = 0
 WHERE litros_consumidos < 0;
 
+ALTER TABLE kickdown
+ADD COLUMN ano_mes VARCHAR(7);
+UPDATE kickdown
+SET ano_mes = DATE_FORMAT(ativado, '%Y%m');
+ALTER TABLE kickdown
+ADD COLUMN grouping_ano_mes VARCHAR(255);
+UPDATE kickdown
+SET grouping_ano_mes = CONCAT(grouping_id, ano_mes);
+
+
 DELETE FROM kickdown
 WHERE ativado is NULL;
