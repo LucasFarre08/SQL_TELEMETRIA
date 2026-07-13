9425dcfd54f0dc588b0a5fb35a78aa11bb1c520d
LucasFarre08 <lucas.farre@buonnytech.com.br>
2026-07-13 14:35:30 -0300

    Update limpeza.sql

--- sql/limpeza.sql
+++ sql/limpeza.sql
@@ -25,3 +25,62 @@ UPDATE kickdown
 SET
     ano_mes = DATE_FORMAT(ativado, '%Y%m'),
     grouping_ano_mes = CONCAT(`grouping`, DATE_FORMAT(ativado, '%Y%m'));
+
+ALTER TABLE kickdown
+ADD COLUMN ano_mes VARCHAR(6);
+
+ALTER TABLE kickdown
+ADD COLUMN grouping_ano_mes VARCHAR(255);
+
+ALTER TABLE freio
+ADD COLUMN ano_mes VARCHAR(6);
+
+ALTER TABLE freio
+ADD COLUMN grouping_ano_mes VARCHAR(255);
+
+ALTER TABLE ociosidade
+ADD COLUMN ano_mes VARCHAR(6);
+
+ALTER TABLE ociosidade
+ADD COLUMN grouping_ano_mes VARCHAR(255);
+
+ALTER TABLE embreagem
+ADD COLUMN ano_mes VARCHAR(6);
+
+ALTER TABLE embreagem
+ADD COLUMN grouping_ano_mes VARCHAR(255);
+
+ALTER TABLE rpm_amarelo
+ADD COLUMN ano_mes VARCHAR(6);
+
+ALTER TABLE rpm_amarelo
+ADD COLUMN grouping_ano_mes VARCHAR(255);
+
+ALTER TABLE rpm_vermelho
+ADD COLUMN ano_mes VARCHAR(6);
+
+ALTER TABLE rpm_vermelho
+ADD COLUMN grouping_ano_mes VARCHAR(255);
+
+ALTER TABLE seguranca
+ADD COLUMN ano_mes VARCHAR(6);
+
+ALTER TABLE seguranca
+ADD COLUMN grouping_ano_mes VARCHAR(255);
+
+ALTER TABLE velocidade_80km
+ADD COLUMN ano_mes VARCHAR(6);
+
+ALTER TABLE velocidade_80km
+ADD COLUMN grouping_ano_mes VARCHAR(255);
+
+ALTER TABLE velocidade_chuva_60km
+ADD COLUMN ano_mes VARCHAR(6);
+
+ALTER TABLE velocidade_chuva_60km
+ADD COLUMN grouping_ano_mes VARCHAR(255);
+
+ALTER TABLE agregado_mensal
+ADD COLUMN grouping_ano_mes VARCHAR(255);
+
+
