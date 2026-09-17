650499b24b416e518fdd837de6e3467a74a470b2
LucasFarre08 <lucas.farre@buonnytech.com.br>
2026-09-17 13:20:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,10 @@ arquivos = [
     "sql/agregado_mensal.sql",
     "sql/agregado_motoristas.sql",
     "sql/agregado_ociosidade.sql",
-    "sql/agregado_mensal_kickdown.sql"
+    "sql/agregado_mensal_kickdown.sql",
+    "sql/agregado_motorista_semanal_west.sql",
+    "sql/agregado_semanal_ociosidade_west.sql",
+    "sql/agregado_semanal_west.sql"
 ]
 
 try:
